5348bcb8287ca6094f4a8a8dd67c49b7336f0e04
Charan <58394919+charanpy@users.noreply.github.com>
2022-04-29 09:51:20 +0530

    changed enrol course

--- controllers/enrol-course/index.js
+++ controllers/enrol-course/index.js
@@ -49,6 +49,9 @@ const getCourseEnrolRequest = catchAsync(async (req, res) => {
   if (req.query?.pending) {
     filters['access'] = req.query?.pending;
   }
+  if (req.query?.role) {
+    filter['role'] = req.query.role;
+  }
 
   const courseEnroll = await EnrolCourse.find(filters);
 
