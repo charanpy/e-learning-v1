73cf96c9d6368ca8742a793296f1aa307ed7df02
Charan <58394919+charanpy@users.noreply.github.com>
2022-04-29 09:31:16 +0530

    added course video

--- routes/videos.route.js
+++ routes/videos.route.js
@@ -6,12 +6,15 @@ const {
   getDeletedVideos,
   deleteVideo,
   getVideoUploadUrl,
+  getVideoForCourse,
 } = require('../controllers/video');
 
 const router = express.Router();
 const { upload } = require('../lib/multer');
+const { checkToken } = require('../services/auth');
 
 router.route('/').post(upload().single('videoThumbnail'), createVideo);
+router.route('/course/:courseId').get(checkToken, getVideoForCourse);
 router.route('/upload').post(getVideoUploadUrl);
 router.route('/').get(getVideos);
 router.route('/:id').get(getVideoById).delete(deleteVideo);
